84762214a19f9a8aacf68e5fbf7c3299359d3389
peakroofingusa <emayainc1@gmail.com>
2026-04-18 14:31:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* Google Analytics 4 */}
+        {/* GA4 */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-TLC51W729X"
           strategy="afterInteractive"
@@ -34,6 +34,30 @@ export default function RootLayout({
             gtag('config', 'G-TLC51W729X');
           `}
         </Script>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1496703165442728');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1496703165442728&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
